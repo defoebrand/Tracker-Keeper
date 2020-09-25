b57e2eeac48eaaa20ee1ebce9fd6063a2bd6987f
Defoe.Brand <defoe.brand@gmail.com>
2020-09-25 22:28:42 +0300

    Generate Users / Transactions / Groups

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :groups
+  resources :transactions
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
