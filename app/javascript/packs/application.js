4f35ad71f2f80c50e7e2bdc093116d62929d4f5c
Defoe.Brand <defoe.brand@gmail.com>
2020-09-29 16:29:42 +0300

    Add jQuery and attempt color scheme switcher

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,8 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+// location.reload();
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
@@ -24,11 +26,11 @@ require("channels")
 // });
 // });
 
-$(document).ready(function() {
-  $('#select_days').change(function() {
-    $('#text_to_be_changed').html($(this).val());
-  });
-});
+// $(document).ready(function() {
+//   $('#select_days').change(function() {
+//     $('#text_to_be_changed').html($(this).val());
+//   });
+// });
 
 // $(document).ready(function(){
 //     $('#select_days').change(function() {
@@ -43,5 +45,74 @@ $(document).ready(function() {
 // $('#clickHereNext').click(function (event) {
 //   $('#number_to_be_changed').html('<%# @click = @click1 %>');
 //   // alert('Hooray!');
+//   event.preventDefault(); // Prevent link from following its href
+// });
+
+// $(document).ready(function() {
+//   $('.burger-menu').click(function() {
+//     $('.burger-links').toggleClass('visible');
+//     alert('yay!');
+//   });
+// });
+//
+// $(document).ready(function() {
+//   $('.parent').click(function() {
+//     $('.sub-nav').toggleClass('visible');
+//   });
+// });
+
+
+// var ready;
+// ready = function() {
+//   $('#nav').localScroll({
+//     duration: 800
+//   });
+// };
+//
+// $(document).ready(ready);
+// $(document).on('page:load', ready);
+
+
+// var ready;
+// ready = function() {
+
+document.addEventListener("turbolinks:load", function() {
+
+  $(document).ready(function() {
+    $('.burger-menu').click(function(event) {
+      $('.hidden').toggleClass('visible');
+      // alert('Hooray!');
+      // event.preventDefault(); // Prevent link from following its href
+    });
+    $('.account-menu').click(function(event) {
+      $('.ac-hidden').toggleClass('ac-visible');
+      // alert('Hooray!');
+      // event.preventDefault(); // Prevent link from following its href
+    });
+  });
+});
+
+// document.addEventListener("turbolinks:load", function() {
+//   $(document).ready(function() {
+//     $('.account-menu').click(function(event) {
+//       $('.hidden').toggleClass('visible');
+//       // alert('Hooray!');
+//       // event.preventDefault(); // Prevent link from following its href
+//     });
+//   });
+// });
+
+
+
+
+// };
+
+// $(document).ready(ready);
+// $(document).on('page:load', ready);
+
+
+
+// $('#my-link').click(function(event) {
+//   alert('Hooray!');
 //   event.preventDefault(); // Prevent link from following its href
 // });
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_many :transactions
   validates :name, uniqueness: true
   has_one_attached :avatar
+
+  def name=(value)
+    super(value.try(:strip))
+  end
 end
