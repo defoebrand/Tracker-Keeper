f3232072bae1167f3c10e9e9f389b80ae1613688
Defoe.Brand <defoe.brand@gmail.com>
2020-10-02 18:48:22 +0300

    Remove zoom login until future update

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   # provider :developer unless Rails.env.production?
-  provider :zoom, ENV['ZOOM_APP_KEY'], ENV['ZOOM_APP_SECRET']
-  # provider :linkedin, 'consumer_key', 'consumer_secret'
   provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], scope: 'user:email'
 end
