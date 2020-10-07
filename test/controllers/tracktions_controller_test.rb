22d6ded7fea2fdd1eaf960dbfd4db67afbb6285b
Defoe.Brand <defoe.brand@gmail.com>
2020-10-07 17:05:48 +0300

    Adjust code per rubocop and sytlelint

--- test/controllers/tracktions_controller_test.rb
+++ test/controllers/tracktions_controller_test.rb
@@ -5,17 +5,17 @@ class TracktionsControllerTest < ActionDispatch::IntegrationTest
     @tracktion = tracktions(:one)
   end
 
-  test "should get index" do
+  test 'should get index' do
     get tracktions_url
     assert_response :success
   end
 
-  test "should get new" do
+  test 'should get new' do
     get new_tracktion_url
     assert_response :success
   end
 
-  test "should create tracktion" do
+  test 'should create tracktion' do
     assert_difference('Tracktion.count') do
       post tracktions_url, params: { tracktion: { amount: @tracktion.amount, author_id: @tracktion.author_id, name: @tracktion.name, type_id: @tracktion.type_id } }
     end
@@ -23,22 +23,22 @@ class TracktionsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to tracktion_url(Tracktion.last)
   end
 
-  test "should show tracktion" do
+  test 'should show tracktion' do
     get tracktion_url(@tracktion)
     assert_response :success
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     get edit_tracktion_url(@tracktion)
     assert_response :success
   end
 
-  test "should update tracktion" do
+  test 'should update tracktion' do
     patch tracktion_url(@tracktion), params: { tracktion: { amount: @tracktion.amount, author_id: @tracktion.author_id, name: @tracktion.name, type_id: @tracktion.type_id } }
     assert_redirected_to tracktion_url(@tracktion)
   end
 
-  test "should destroy tracktion" do
+  test 'should destroy tracktion' do
     assert_difference('Tracktion.count', -1) do
       delete tracktion_url(@tracktion)
     end
--- test/system/tracktions_test.rb
+++ test/system/tracktions_test.rb
@@ -1,49 +1,49 @@
-require "application_system_test_case"
+require 'application_system_test_case'
 
 class TracktionsTest < ApplicationSystemTestCase
   setup do
     @tracktion = tracktions(:one)
   end
 
-  test "visiting the index" do
+  test 'visiting the index' do
     visit tracktions_url
-    assert_selector "h1", text: "Tracktions"
+    assert_selector 'h1', text: 'Tracktions'
   end
 
-  test "creating a Tracktion" do
+  test 'creating a Tracktion' do
     visit tracktions_url
-    click_on "New Tracktion"
+    click_on 'New Tracktion'
 
-    fill_in "Amount", with: @tracktion.amount
-    fill_in "Author", with: @tracktion.author_id
-    fill_in "Name", with: @tracktion.name
-    fill_in "Type", with: @tracktion.type_id
-    click_on "Create Tracktion"
+    fill_in 'Amount', with: @tracktion.amount
+    fill_in 'Author', with: @tracktion.author_id
+    fill_in 'Name', with: @tracktion.name
+    fill_in 'Type', with: @tracktion.type_id
+    click_on 'Create Tracktion'
 
-    assert_text "Tracktion was successfully created"
-    click_on "Back"
+    assert_text 'Tracktion was successfully created'
+    click_on 'Back'
   end
 
-  test "updating a Tracktion" do
+  test 'updating a Tracktion' do
     visit tracktions_url
-    click_on "Edit", match: :first
+    click_on 'Edit', match: :first
 
-    fill_in "Amount", with: @tracktion.amount
-    fill_in "Author", with: @tracktion.author_id
-    fill_in "Name", with: @tracktion.name
-    fill_in "Type", with: @tracktion.type_id
-    click_on "Update Tracktion"
+    fill_in 'Amount', with: @tracktion.amount
+    fill_in 'Author', with: @tracktion.author_id
+    fill_in 'Name', with: @tracktion.name
+    fill_in 'Type', with: @tracktion.type_id
+    click_on 'Update Tracktion'
 
-    assert_text "Tracktion was successfully updated"
-    click_on "Back"
+    assert_text 'Tracktion was successfully updated'
+    click_on 'Back'
   end
 
-  test "destroying a Tracktion" do
+  test 'destroying a Tracktion' do
     visit tracktions_url
     page.accept_confirm do
-      click_on "Destroy", match: :first
+      click_on 'Destroy', match: :first
     end
 
-    assert_text "Tracktion was successfully destroyed"
+    assert_text 'Tracktion was successfully destroyed'
   end
 end
